c8891d0520cabb7ffe2d46c6f3dc9812e437b6e9
Natasha Petrus <hello@natashapetrus.com>
2017-09-02 16:16:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
   var newArray = array;
-  for (let i = 0; i<25, i++){
+  for (let i = 0; i<25; i++){
     newArray = [...newArray,  `"I am ${i} strange loop." `];
   }
   return newArray;
